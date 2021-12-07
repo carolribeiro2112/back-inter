9366388c61089fd42d5d7daa56e074746682b939
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-12-07 16:50:35 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3333;
+
+app.get('/', (req, res) => res.send('Express + Typescript'))
+  
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`); 
+});
