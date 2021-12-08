b3c50cabed5814716e095d19782197974a30cb42
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-12-08 17:11:03 -0300

    encode/decode pix

--- src/routes/pix.routes.ts
+++ src/routes/pix.routes.ts
@@ -1,7 +1,10 @@
 import { Router }  from "express";
+import userAuthenticated from "../middlewares/userAuthenticated";
  
 const pixRouter = Router(); 
 // const pixController = new PixController();
+
+pixRouter.use(userAuthenticated)
 
 // userRouter.post('/signin', userController.signin)
 // userRouter.post('/signup', userController.signup)
