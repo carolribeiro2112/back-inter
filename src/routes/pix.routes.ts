79c09cc8a1c66cfe2d15080d0a338c208fd4cf72
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-12-08 16:53:24 -0300

    authentication middleware

---
+++ src/routes/pix.routes.ts
@@ -0,0 +1,10 @@
+import { Router }  from "express";
+ 
+const pixRouter = Router(); 
+// const pixController = new PixController();
+
+// userRouter.post('/signin', userController.signin)
+// userRouter.post('/signup', userController.signup)
+// userRouter.get('/signup', userController.signup)
+
+export default pixRouter;
